eeb474222d4930ab1a8e4367919d2a757067cc30
Josh Stow <joshxstow@gmail.com>
2021-08-06 15:21:28 +0100

    create RadPanda contract

---
+++ blockchain/contracts/RadPanda.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ERC721Tradable.sol";
+
+/// @title RadPanda ERC-721 token contract.
+contract RadPanda is ERC721Tradable {
+    constructor(address _proxyRegistryAddress)
+        ERC721Tradable("RadPanda", "PANDA", _proxyRegistryAddress)
+    {}
+
+    function baseTokenURI() override public pure returns (string memory) {
+        return "https://animal-kingdom.rad.live/pandas/";
+    }
+
+    function contractURI() public pure returns (string memory) {
+        return "https://www.radkingdom.org/";
+    }
+}
