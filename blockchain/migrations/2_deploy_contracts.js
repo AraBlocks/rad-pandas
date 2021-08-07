9637395a9438db6f538a04d24aecfad01efa82b5
Josh Stow <joshxstow@gmail.com>
2021-08-07 21:43:38 +0100

    update migrations file

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -2,6 +2,16 @@ const RadPanda = artifacts.require("RadPanda");
 const RadPandaFactory = artifacts.require("RadPandaFactory");
 
 module.exports = function (deployer) {
-  deployer.deploy(RadPanda);
-  deployer.deploy(RadPandaFactory);
+  let proxyRegistryAddress = "";
+  if (network === 'rinkeby') {
+    proxyRegistryAddress = "0xf57b2c51ded3a29e6891aba85459d600256cf317";
+  } else {
+    proxyRegistryAddress = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
+  }
+
+  deployer.deploy(RadPanda, proxyRegistryAddress);
+  deployer.deploy(RadPandaFactory, proxyRegistryAddress, RadPanda.address);
+
+  const RadPanda = await RadPanda.deployed();
+  await RadPanda.transferOwnership(RadPandaFactory.address);
 };
