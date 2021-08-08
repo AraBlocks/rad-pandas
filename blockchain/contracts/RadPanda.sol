206c6194df3846b3c82139e94a1c84edada0c7b9
Josh Stow <joshxstow@gmail.com>
2021-08-08 01:40:31 +0100

    add initial mint function

--- blockchain/contracts/RadPanda.sol
+++ blockchain/contracts/RadPanda.sol
@@ -5,8 +5,9 @@ import "./ERC721Tradable.sol";
 
 /// @title RadPanda ERC-721 token contract.
 contract RadPanda is ERC721Tradable {
-  constructor(address _proxyRegistryAddress)
+  constructor(address _proxyRegistryAddress, uint256 numTokens, address initialMintAddress)
     ERC721Tradable("RadPanda", "PANDA", _proxyRegistryAddress)
+    _initialMint(numTokens, initialMintAddress);
   {}
 
   function baseTokenURI() override public pure returns (string memory) {
@@ -15,5 +16,11 @@ contract RadPanda is ERC721Tradable {
 
   function contractURI() public pure returns (string memory) {
     return "https://www.radkingdom.org/";
+  }
+
+  function _initialMint(uint256 _numTokens, address _initialMintAddress) private {
+    for (uint256 i=0; i<numTokens; i++) {
+      mintTo(_initialMintAddress);
+    }
   }
 }
