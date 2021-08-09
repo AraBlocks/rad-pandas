d9816aeaeffc2fd2fb3b751e5877f34ef7fa1860
Josh Stow <joshxstow@gmail.com>
2021-08-09 18:19:10 +0100

    update truffle config

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -1,9 +1,6 @@
 const RadPanda = artifacts.require("RadPanda");
 const RadPandaFactory = artifacts.require("RadPandaFactory");
 
-const numTokens = 25;
-const initialMintAddress = "0x9da6382Fa5EE8c8ec691F56764B017D8EE57C9d9";
-
 module.exports = async (deployer, network) => {
   let proxyRegistryAddress = "";
   if (network === 'rinkeby') {
@@ -12,7 +9,7 @@ module.exports = async (deployer, network) => {
     proxyRegistryAddress = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
   }
 
-  await deployer.deploy(RadPanda, proxyRegistryAddress, numTokens, initialMintAddress, {gas: 7000000});
+  await deployer.deploy(RadPanda, proxyRegistryAddress, {gas: 7000000});
   await deployer.deploy(RadPandaFactory, proxyRegistryAddress, RadPanda.address, {gas: 3000000});
 
   const radPanda = await RadPanda.deployed();
